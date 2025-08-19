62139b0d478503ae0f413949cd6ecb990ebba9c6
Ryuji Eguchi <jp.ryuji@gmail.com>
2025-08-19 19:21:10 +1000

    fix: add ConfigModule to AppModule to resolve dependency injection issue

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -12,6 +13,9 @@ import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     CommonModule,
     MiddlewareModule,
     PrismaModule,
